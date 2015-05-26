3573799d5d8d9c398ef18dafb142791c898eb473
KSouthwood <keri.southwood@gmail.com>
2015-05-26 13:17:34 -0400

    Making progress....
    - Completed import successfully.
    Biggest hurdle was getting the SQL syntax correct, otherwise completed the program in about 15 minutes.
    
    - Completed search.php
    Hardest part here so far was getting the SQL syntax correct - again. After spending a lot of searching and reading understanding how the $_GET variable works and how I wanted to use it for the SQL search.

--- CS50_Mashup/public/search.php
+++ CS50_Mashup/public/search.php
@@ -6,7 +6,17 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"]
-
+    
+	// break geo out into components...
+	$search = array(",", "+");
+	$geo = str_replace($search, " ", $_GET["geo"]);
+	
+	$rows = query("SELECT * FROM `places` WHERE MATCH(postal_code,place_name,admin_name1,admin_code1) against (? IN NATURAL LANGUAGE MODE)", $geo);
+	if ($rows !== FALSE)
+	{
+		$places = $rows;
+	}
+	
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
