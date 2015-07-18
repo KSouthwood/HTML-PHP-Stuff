625f321ecd84ba3f31ee599460a530486e9fccfa
KSouthwood <keri.southwood@gmail.com>
2015-07-18 10:06:22 -0400

    - Made bounding box of mine a square just larger than the mine so the image would fill the square on the grid better
    Changes to scripts.js:
    - Changed value of BOX_PXL for better size on the web-page
    - Added minecount array
    - Added list of colors to use for how many mines there are around a cell
    - Added a FlagStatus and flags array
    - Changed name of variable 'gameboard' to 'minefield' (better suits the game and easier to know what it is)
    - changed several functions to now accept the canvas context rather than the canvas itself
    - New function handleClick() to call correct function depending if user ctrl-clicked or not
    
    Changes to initialize():
    - update onclick handler with new function name (handleClick)
    - changed name of canvas sizing function to better reflect it can be re-used (i.e. when size of minefield becomes user-configurable)
    - added drawing of canvas background to simulate grid lines
    
    Changes to index.html:
    - added hidden div to pre-load image of mine

--- Minestepper/js/scripts.js
+++ Minestepper/js/scripts.js
@@ -17,7 +17,7 @@
  */
 
 // size of boxes to draw on canvas in pixels
-var BOX_PXL = 20;
+var BOX_PXL = 25;
 // size of padding between boxes in pixels
 var PADDING = 2;
 var OFFSET = BOX_PXL + PADDING;
@@ -29,7 +29,7 @@ var OFFSET = BOX_PXL + PADDING;
 
 /**
  * Size is Natural
- * interp. size of gameboard
+ * interp. size of minefield
  */
 
 var SIZE = 10;
@@ -45,12 +45,20 @@ var MA = true;
 var MI = false;
 
 /**
- * Field is Array of Mine
+ * Minefield is Array of Mine
  * interp. array to hold grid of mine locations
  */
 
-var F1 = [[MA, MI], [MI, MA]];
+var MF1 = [[MA, MI], [MI, MA]];
+var minefield;
 
+/**
+ * Minecount is Array of Natural
+ * interp. array to hold count of mines around a given Minefield cell
+ */
+
+var MC1 = [[0, 2], [0, 1]];
+var minecount;
 /**
  * Score is Natural
  * interp. count of mines found
@@ -61,28 +69,141 @@ var SC1 = 5;
 var SC2 = 10;
 
 /**
+ *  Colors is Enumeration
+ *  interp. list of colors to use for displaying number of mines
+ */
+
+var colors = ['cyan', 'lightskyblue', 'deepskyblue', 'blue', 'darkblue', 'forestgreen', 'darkgreen', 'orangered', 'red'];
+
+/**
+ *  FlagStatus is Enumeration
+ *  interp. players status of what's in a square
+ */
+
+var FS0 = "blank";
+var FS1 = "maybe";
+var FS2 = "definite";
+
+/**
+ *  Flags is Array of FlagStatus
+ *  array to hold the players status of what's in a square
+ */
+var F1 = [[FS0, FS1], [FS2, FS0]];
+var flags;
+
+/**
  *  Function Definitions
  *  ====================
  */
 
 /**
- *  Initialize variables and gameboard
+ *  Initialize variables and minefield
  *  !!! - write function
  */
 function initialize() {
 	var canvas = document.getElementById('gameboard');
 	var ctx = canvas.getContext('2d');
-	canvas.addEventListener('mousedown', mouseClick, false);
-	fitToContainer(canvas);
-	drawInitGameboard(canvas, SIZE);
-	gameboard = initArray(SIZE, MI);
-	populateGameboard(gameboard);
-	minecount = findMines(gameboard);
+	canvas.addEventListener('click', handleClick, false);
+	resizeCanvas(canvas);
+	ctx.fillStyle = "lightgray";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	drawInitMinefield(ctx, SIZE);
+	minefield = initArray(SIZE, MI);
+	populateMinefield(minefield);
+	minecount = findMines(minefield);
+	flags = initArray(SIZE, FS0);
+}
+
+/**
+ *  Event
+ *  handles mouse click and calls appropriate function based on state of ctrl key
+ * 
+ */
+
+function handleClick(event) {
+	if (event.ctrlKey) {
+		singleClick(event);
+	} else {
+		doubleClick(event);
+	}
+}
+/**
+ *  Event -> Image
+ *  handles double click on the canvas - reveals whats behind the square
+ * !!! - not working - col keeps being off by 1
+ */
+function doubleClick(event) {
+	var canvas = document.getElementById("gameboard");
+	var ctx = canvas.getContext('2d');
+	var rc = getRowCol(canvas, event);
+
+	clearRect(ctx, rc.row, rc.col);
+	if (minefield[rc.row][rc.col]) {
+		drawMine(ctx, rc.row, rc.col);
+	} else {
+		safeStep(ctx, rc.row, rc.col);
+	}
+}
+
+/**
+ * 	Event -> Image
+ *  changes the square status from blank to question mark and back
+ */
+
+function singleClick(event) {
+	var canvas = document.getElementById("gameboard");
+	var context = canvas.getContext('2d');
+	var rc = getRowCol(canvas, event);
+
+	// use switch/case statement??
+	switch (flags[rc.row][rc.col]) {
+	// if current status is blank, update to maybe and display question mark on top of square
+	case FS0:
+		flags[rc.row][rc.col] = FS1;
+		drawRect(context, 'gray', rc.row, rc.col);
+		drawNumber(context, rc.row, rc.col, "?", 'black');
+		break;
+	// if current status is maybe, update to definite and display flag on top of square
+	case FS1:
+		flags[rc.row][rc.col] = FS2;
+		drawRect(context, 'gray', rc.row, rc.col);
+		drawMine(context, rc.row, rc.col);
+		break;
+	// if current status is definite, update to blank and redraw blank square
+	case FS2:
+	default:
+		flags[rc.row][rc.col] = FS0;
+		drawRect(context, 'gray', rc.row, rc.col);
+		break;
+	}
+}
+
+/**
+ *  Canvas -> Canvas
+ *  calculates the size of the canvas for the minefield
+ *
+ */
+function resizeCanvas(canvas) {
+	canvas.width = (SIZE * BOX_PXL) + ((SIZE + 1) * PADDING);
+	canvas.height = canvas.width;
+}
+
+/**
+ *  Canvas Natural -> Image
+ *  draws the boxes on the minefield
+ */
+
+function drawInitMinefield(context, size) {
+	for (var r = 0; r < SIZE; r++) {
+		for (var c = 0; c < SIZE; c++) {
+			drawRect(context, 'gray', r, c);
+		};
+	};
 }
 
 /**
  *  Natural Number/String/Boolean -> Array
- *  defines the array for the gameboard and fills it with a default value
+ *  defines the array for the minefield and fills it with a default value
  *
  */
 function initArray(size, fill) {
@@ -98,35 +219,12 @@ function initArray(size, fill) {
 }
 
 /**
- *  Canvas -> Canvas
- *  calculates the size of the canvas for the gameboard
+ *  Array -> Array
+ *  place mines randomly on the minefield
  *
  */
-function fitToContainer(canvas) {
-	canvas.width = (SIZE * BOX_PXL) + ((SIZE + 1) * PADDING);
-	canvas.height = canvas.width;
-}
-
-/**
- *  Canvas Natural -> Image
- *  draws the boxes on the gameboard
- */
-
-function drawInitGameboard(context, size) {
-	for (var i = 0; i < SIZE; i++) {
-		for (var j = 0; j < SIZE; j++) {
-			addRect(context, 'gray', i, j);
-		};
-	};
-}
-
-/**
- *  Array -> Array
- *  place mines randomly on the gameboard
- * 
- */
-function populateGameboard(minefield) {
-	// do mines in 20% of the gameboard
+function populateMinefield(minefield) {
+	// do mines in 20% of the minefield
 	var mines = Math.floor((SIZE * SIZE) * 0.2);
 	while (mines > 0) {
 		var placeMine = Math.floor(Math.random() * (SIZE * SIZE));
@@ -141,19 +239,19 @@ function populateGameboard(minefield) {
 
 /**
  *  Array -> Array
- *  iterate through gameboard and count up mines around each cell
+ *  iterate through minefield and count up mines around each cell
  */
 function findMines(minefield) {
 	// create array for mine count
 	var mc = initArray(SIZE, 0);
 	var xlim = SIZE - 1;
 	var ylim = SIZE - 1;
-	
-	// 
+
+	//
 	for (var row = 0; row < SIZE; row++) {
 		for (var col = 0; col < SIZE; col++) {
 			// check all neighbors of minefield[row][col]
-			for (var x = Math.max(0, row-1); x <= Math.min(row + 1, xlim); x++) {
+			for (var x = Math.max(0, row - 1); x <= Math.min(row + 1, xlim); x++) {
 				for (var y = Math.max(0, col - 1); y <= Math.min(col + 1, ylim); y++) {
 					if (x !== row || y !== col) {
 						if (minefield[x][y]) {
@@ -164,35 +262,8 @@ function findMines(minefield) {
 			}
 		}
 	}
-	
+
 	return mc;
-}
-
-/**
- *  Event -> ??
- *  handle mouse click on the canvas
- * !!! - not working - col keeps being off by 1
- */
-function mouseClick(event) {
-	var x = event.x;
-	var y = event.y;
-	var canvas = document.getElementById("gameboard");
-	// subtracting 17 to accomodate row margin and border
-	var xx = x - canvas.offsetParent.offsetLeft - 17;
-	var yy = y - canvas.offsetParent.offsetTop;
-
-	// convert (x,y) into (row,col) of square that was clicked
-	var row = Math.floor(yy / OFFSET);
-	var col = Math.floor(xx / OFFSET);
-	console.log('Clicked: {', x, ',', y, '} Canvas point: {', xx, ',', yy, '} Gameboard {', row, ',', col, '}');
-	addRect(canvas, 'white', row, col);
-	if (gameboard[row][col]) {
-		ctx = canvas.getContext('2d');
-		drawMine(ctx, row, col);
-	}
-	else {
-		drawNumber(canvas, row, col);
-	}
 }
 
 /**
@@ -200,30 +271,88 @@ function mouseClick(event) {
  *  adds a rectangle to the canvas
  */
 
-function addRect(context, color, r, c) {
+function drawRect(context, color, row, col) {
 	context.fillStyle = color;
-	context.fillRect(rctop(c), rctop(r), BOX_PXL, BOX_PXL);
+	context.fillRect(rctop(col), rctop(row), BOX_PXL, BOX_PXL);
+}
+
+/**
+ *  Canvas String Natual Natural -> Image
+ *  adds a rectangle to the canvas
+ */
+
+function clearRect(context, row, col) {
+	context.clearRect(rctop(col), rctop(row), BOX_PXL, BOX_PXL);
 }
 
 /**
  * Canvas Natural Natural -> Image
  * draws the image of the mine onto the canvas at the given row and column
- * 
+ *
  */
-// !!! write function
-function drawMine(canvas, row, col) {
-	mineImg = new Image();
-	mineImg.src = "./img/mine.svg";
-	mineImg.onLoad = function () {
-		context.drawImage(mineImg, (row * OFFSET + PADDING), (col * OFFSET + PADDING), BOX_PXL, BOX_PXL);
-	}
+// !!! image drawing to canvas, just very small. have to use Inkscape to change bounding box size??
+function drawMine(context, row, col) {
+	context.drawImage(mineImg, rctop(col), rctop(row), BOX_PXL, BOX_PXL);
+}
+
+/**
+ *  Context Natural Natural -> Image
+ *  draws the number of mines around a box as an image
+ *
+ */
+
+function drawNumber(context, row, col, character, color) {
+	context.fillStyle = color;
+	context.font = 'bold ' + (BOX_PXL - (PADDING * 2)) + 'px san-serif';
+	context.textAlign = 'center';
+	context.textBaseline = 'middle';
+	context.fillText(character, (rctop(col) + (BOX_PXL / 2)), (rctop(row) + (BOX_PXL / 2)));
+}
+
+/**
+ *  Event -> Natural
+ */
+function getRowCol(canvas, event) {
+	var clicked = {
+		row : 0,
+		col : 0
+	};
+
+	// subtracting 15 to accomodate row margin and border
+	var xx = (event.x - canvas.offsetParent.offsetLeft - 15);
+	var yy = (event.y - canvas.offsetParent.offsetTop);
+
+	// convert (x,y) into (row,col) of square that was clicked
+	clicked.row = Math.floor(yy / OFFSET);
+	clicked.col = Math.floor(xx / OFFSET);
+
+	console.log('Clicked: {', event.x, ',', event.y, '} Canvas point: {', xx, ',', yy, '} Gameboard {', clicked.row, ',', clicked.col, '}');
+
+	return clicked;
 }
 
 /**
  *  Natural -> Natural
- *  calculates the pixel offset for a specified row or column
+ *  calculates the upper left pixel offset for a specified row or column
  */
 
 function rctop(n) {
-	return (PADDING + (n * BOX_PXL) + (n * PADDING))
+	return (PADDING + (n * BOX_PXL) + (n * PADDING));
+}
+
+/**
+ *  Context Natural Natural -> ??
+ *  safe step (found no mine), now we reveal how many mines are around us
+ */
+
+function safeStep(context, row, col) {
+	// one or more mines as a neighbor, so just print how many and return
+	if (minefield[row, col] != 0) {
+		drawNumber(context, row, col, minecount[row][col], colors[minecount[row][col]]);
+		return;
+	}
+
+	// no mines as a neighbor, so check if any other neighbors have zero as well and clear those cells
+	drawNumber(context, row, col, minecount[row][col], colors[minecount[row][col]]);
+	// TODO - remove (just added in so routine will work for now...)
 }
